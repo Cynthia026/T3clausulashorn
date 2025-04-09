99b4d99cf62361ea74e9137801009d7bb9428be7
Cynthia026 <cm2323263@gmail.com>
2025-04-08 23:24:29 -0600

    Version 1

--- T3cadalim.pl
+++ T3cadalim.pl
@@ -4,6 +4,6 @@ come(raton, queso).
 es_animal(gato).
 es_animal(raton).
 es_animal(queso). % Solo como ejemplo, aunque queso no es animal
-% Reglas
+% Reglas de inferencia
 come(gato,queso) :- come(gato,raton),come (raton,queso).
 come(gato,raton) :- come(gato,raton), come(raton,queso).
