9fb4fa368ab87a0cfada224f6e36751d5d3ff422
Cynthia026 <cm2323263@gmail.com>
2025-04-08 23:21:13 -0600

    Version Inicial

---
+++ T3cadalim.pl
@@ -0,0 +1,9 @@
+% Hechos
+come(gato, raton).
+come(raton, queso).
+es_animal(gato).
+es_animal(raton).
+es_animal(queso). % Solo como ejemplo, aunque queso no es animal
+% Reglas
+come(gato,queso) :- come(gato,raton),come (raton,queso).
+come(gato,raton) :- come(gato,raton), come(raton,queso).
